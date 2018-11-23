db87cca224de6c994416364cc1c597787dc6afcc
Bear Kent <davidbearkent@gmail.com>
2018-11-23 22:59:21 +0000

    Squashed commit of the following:
    commit 5cf1feb81c3a8dec5468b175ad6f3d9cf8f71dd5
    Author: Bear Kent <davidbearkent@gmail.com>
    Date:   Fri Nov 23 22:52:26 2018 +0000
    
        deleted neural network and added download neural network because github was unhappy with the size of the neural net file
    
    commit cf0bb369a65c0a662f0116cc5c447258fae10b22
    Author: Bear Kent <davidbearkent@gmail.com>
    Date:   Fri Nov 23 22:43:21 2018 +0000
    
        install python and first image detection

---
+++ download_resnet.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/resnet50_coco_best_v2.0.1.h5
---
+++ install_python.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PIP=pip-3.6
+
+${PIP} install tensorflow
+${PIP} install numpy
+${PIP} install scipy
+${PIP} install opencv-python
+${PIP} install pillow
+${PIP} install matplotlib
+${PIP} install h5py
+${PIP} install keras
+${PIP} install https://github.com/OlafenwaMoses/ImageAI/releases/download/2.0.2/imageai-2.0.2-py3-none-any.whl
