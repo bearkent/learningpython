05aa57c204c9d128e345923efdf75c1bc9b16429
Bear Kent <davidbearkent@gmail.com>
2018-12-01 21:17:03 +0000

    added deer detection code with camera code in it

--- deer_detection2.py
+++ deer_detection2.py
@@ -1,10 +1,14 @@
 from imageai.Detection import ObjectDetection
 import os
 import glob
+from picamera import PiCamera
+from time import sleep
 
 execution_path = os.getcwd()
 
 print("execution_path=",execution_path)
+
+camera = PiCamera()
 
 detector = ObjectDetection()
 detector.setModelTypeAsRetinaNet()
@@ -13,6 +17,12 @@ detector.loadModel()
 
 print("detector set up")
 
+def capture_image(file):
+    camera.start_preview
+    sleep(2)
+    camera.capture(file)
+    camera.stop_preview
+    
 def image_detection(detector, input_image, out_image):
     detections = detector.detectObjectsFromImage(input_image=input_image, output_image_path=out_image)
 
